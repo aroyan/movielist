259e6599fc5931ffcd93a31befae57e30ea5ec36
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-10-20 10:58:16 +0800

    refactor: use rtk query for search movie/tv

--- src/features/movie/movieSlice.js
+++ src/features/movie/movieSlice.js
@@ -1,23 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+
 export const movieApi = createApi({
   reducerPath: 'movieApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3/' }),
   endpoints: (builder) => ({
     getAllMovies: builder.query({
-      query: (page = 1) => `movie/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}&page=${page}`,
+      query: (page = 1) => `movie/popular?api_key=${apiKey}&page=${page}`,
     }),
     getAllSeries: builder.query({
-      query: (page = 1) => `tv/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}&page=${page}`,
+      query: (page = 1) => `tv/popular?api_key=${apiKey}&page=${page}`,
     }),
     getWeeklyTrending: builder.query({
-      query: () => `trending/all/week?api_key=${import.meta.env.VITE_TMDB_API_KEY}`,
+      query: () => `trending/all/week?api_key=${apiKey}`,
     }),
     getDetailData: builder.query({
-      query: ({ media, id }) => `${media}/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}`,
+      query: ({ media, id }) => `${media}/${id}?api_key=${apiKey}`,
     }),
     getTrailer: builder.query({
-      query: ({ media, id }) => `${media}/${id}/videos?api_key=${import.meta.env.VITE_TMDB_API_KEY}`,
+      query: ({ media, id }) => `${media}/${id}/videos?api_key=${apiKey}`,
+    }),
+    // prettier-ignore
+    getSearchData: builder.query({
+      query: ({ q, page = 1 }) => `search/multi?api_key=${apiKey}&language=en-US&page=${page}&include_adult=false&query=${q}`,
     }),
   }),
 });
@@ -28,4 +34,5 @@ export const {
   useGetWeeklyTrendingQuery,
   useGetDetailDataQuery,
   useGetTrailerQuery,
+  useGetSearchDataQuery,
 } = movieApi;
--- src/pages/SearchResult/index.jsx
+++ src/pages/SearchResult/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSearchParams, NavLink } from 'react-router-dom';
 import {
   Box,
@@ -9,13 +9,13 @@ import {
   Image,
   Link,
   Text,
+  //
 } from '@chakra-ui/react';
+
 import Layout from '@/components/Layout';
+import { useGetSearchDataQuery } from '@/features/movie/movieSlice';
 
 function SearchResult() {
-  const [searchResult, setSearchResult] = useState(null);
-  const [totalPages, setTotalPages] = useState(0);
-
   const [searchParams, setSearchParams] = useSearchParams({
     page: 1,
   });
@@ -23,9 +23,11 @@ function SearchResult() {
   const query = searchParams.get('q');
   const page = searchParams.get('page');
 
-  const SEARCH_MOVIE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${
-    import.meta.env.VITE_TMDB_API_KEY
-  }&query=${query}&include_adult=false&page=${page}`;
+  const { data } = useGetSearchDataQuery({ q: query, page });
+
+  const filteredData = data?.results?.filter((item) => item.media_type !== 'person') ?? [];
+
+  const { total_pages: totalPages } = data ?? {};
 
   const handleDecrementPage = () => {
     setSearchParams({
@@ -41,16 +43,6 @@ function SearchResult() {
     });
   };
 
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(SEARCH_MOVIE_URL);
-      const result = await response.json();
-      setSearchResult(result.results);
-      setTotalPages(result.total_pages);
-    };
-    getData();
-  }, [query, page]);
-
   return (
     <Layout>
       <Box as="section" mt="64px" mx="1rem" mb="2rem">
@@ -58,20 +50,10 @@ function SearchResult() {
           Search result for {query}
         </Heading>
 
-        <Flex
-          my="1rem"
-          wrap="wrap"
-          gap={{ base: '1rem', md: '2rem' }}
-          justify="center"
-        >
-          {searchResult?.map((res) => (
-            <Box
-              key={res.id}
-              width={{ base: '150px', md: '200px' }}
-              boxShadow="md"
-              rounded="lg"
-            >
-              <Link to={`/movie/${res.id}`} as={NavLink}>
+        <Flex my="1rem" wrap="wrap" gap={{ base: '1rem', md: '2rem' }} justify="center">
+          {filteredData?.map((res) => (
+            <Box key={res.id} width={{ base: '150px', md: '200px' }} boxShadow="md" rounded="lg">
+              <Link to={`/${res.media_type}/${res.id}`} as={NavLink}>
                 <Image
                   rounded="lg"
                   src={`https://image.tmdb.org/t/p/w500${res.poster_path}`}
@@ -85,16 +67,11 @@ function SearchResult() {
             </Box>
           ))}
         </Flex>
-        {searchResult?.length > 0 ? (
+        {filteredData?.length > 0 ? (
           <Center flexDir="column">
             <Text mb="1rem">Page : {page}</Text>
             <Flex gap="0.5rem">
-              <Button
-                colorScheme="facebook"
-                rounded="full"
-                disabled={page <= 1}
-                onClick={handleDecrementPage}
-              >
+              <Button colorScheme="facebook" rounded="full" disabled={page <= 1} onClick={handleDecrementPage}>
                 {+page === 1 ? '' : +page - 1}
               </Button>
               <Button disabled rounded="full" colorScheme="orange">
@@ -122,19 +99,3 @@ function SearchResult() {
 }
 
 export default SearchResult;
-
-// Todos: Implement button search tv series
-
-// const handleShowTv = () => {
-//   setSearchParams({
-//     q: query,
-//     page: +searchParams.get('page'),
-//   });
-// };
-
-/* <FormControl display="flex" alignItems="center" justifyContent="center">
-          <FormLabel htmlFor="email-alerts" mb="0">
-            Show TV series result instead?
-          </FormLabel>
-          <Switch id="email-alerts" onChange={handleShowTv} />
-        </FormControl> */
