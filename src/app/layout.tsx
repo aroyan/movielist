bea99b89267c9f7ff060bd919754d581f969627f
Aroyan <43630681+aroyan@users.noreply.github.com>
2023-05-05 03:55:26 +0000

    initial layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +12,32 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <Link
+            href="/"
+            className={inter.className}
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+              fontWeight: 'bold',
+            }}
+          >
+            Hello
+          </Link>
+          <Link
+            href="/nothing"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+              fontWeight: 'bold',
+            }}
+          >
+            Nothing
+          </Link>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
-export default function Page() {
+import styles from './page.module.css';
+
+async function getTrending() {
+  const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=eea6cf8e79673e103a0a449bc11cf5ef&page=1');
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getTrending();
+
   return (
     <>
       <article>
         <div>
-          <h2>Popular Movie</h2>
+          {data.results.map((x: any) => (
+            <div
+              className={styles.slide}
+              key={x.id}
+              style={{
+                background: `url("https://image.tmdb.org/t/p/original${x.backdrop_path}")`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                objectFit: 'cover',
+                height: '100vh',
+                position: 'relative',
+              }}
+            >
+              <div>
+                <p
+                  style={{
+                    color: 'white',
+                    fontSize: '1.5rem',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {x.title}
+                </p>
+                <p>{x.overview}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </article>
       <article>
