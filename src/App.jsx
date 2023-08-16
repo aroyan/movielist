23b7ca0323121fa14c874238d12243b6877b7498
Aroyan <43630681+aroyan@users.noreply.github.com>
2023-08-16 05:10:04 +0000

    fix: move noscript to index.html

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,16 @@ import {
 
 function App() {
   return (
-    <>
-      <noscript>
-        <p>Please enable your JavaScript</p>
-      </noscript>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:media/:id" element={(<Detail />)} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/movie" element={<Movie />} />
-        <Route path="/search" element={<SearchResult />} />
-        <Route path="/tv" element={<Tv />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:media/:id" element={(<Detail />)} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/movie" element={<Movie />} />
+      <Route path="/search" element={<SearchResult />} />
+      <Route path="/tv" element={<Tv />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
